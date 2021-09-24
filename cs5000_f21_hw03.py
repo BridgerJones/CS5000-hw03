88e586893b53492492048928f8897bf23c766955
Bridger Jones <bridgerjones15@gmail.com>
2021-09-24 15:51:18 -0600

    working but need to try and get it more like his format if possible

--- cs5000_f21_hw03.py
+++ cs5000_f21_hw03.py
@@ -1,28 +1,57 @@
 
 ###############################################
 # cs5000_f21_hw03.py
-# YOUR NAME
-# YOUR A#
+# Bridger Jones
+# A02314787
 ###############################################
 
 
 def nfa_to_dfa(nfa):
-    ## your code here
-    pass
+    states = nfa[0]
+    sigma = nfa[1]
+    delta = nfa[2]
+    start_state = nfa[3]
+    final_states = nfa[4]
+
+    new_states = []
+    new_delta = {}
+    new_final_states = []
+    new_sigma = sigma
+
+    # initialize queue
+    queue = []
+    queue.append(set([start_state]))
+
+    while len(queue) != 0:
+        current_state = queue.pop(0)
+
+        for alphabet in sigma:
+            new_state = set()
+            for state in current_state:
+                try:
+                    new_state |= delta[state, alphabet]
+                except KeyError:
+                    pass
+            new_state = set(sorted(tuple(new_state)))
+            if new_state not in states and new_state not in new_states:
+                queue.append(new_state)
+                new_states.append(new_state)
+            new_delta[(str(current_state), alphabet)] = new_state
+    for state in new_states:
+        for sub_state in state:
+            if sub_state in final_states:
+                new_final_states.append(state)
+
+    return (new_states, sigma, new_delta, start_state, new_final_states)
+
+
 
 def display_dfa(dfa):
-    ## your code here
-    pass
-
-
-
-
-    
-    
-
-
-
-
-
-
-
+    print(f"STATES: {dfa[0]}")
+    print(f"SIGMA: {dfa[1]}")
+    print(f"START STATE: {dfa[3]}")
+    count = 0
+    for key, value in dfa[2].items():
+        print(f"{count}) {key} = {value}")
+        count += 1
+    print(f"FINAL STATES: {dfa[4]}")
